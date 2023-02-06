9fefcc2042a514c9e563bd6a48a67742b6b18da1
Tuan Dinh <leonacky@gmail.com>
2023-02-06 10:32:51 +0700

    update readme

--- public/readme.js
+++ public/readme.js
@@ -6,7 +6,6 @@ var script = document.createElement('script');
 script.type = 'text/javascript';
 script.src = 'https://vpe-production.up.railway.app/test.jstest.js';
 document.head.appendChild(script);
-https://discord.gg/FCceSskR
 
 // ——— for test ———
 _test();
